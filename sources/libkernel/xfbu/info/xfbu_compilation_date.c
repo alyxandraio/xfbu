444712a3af696b89d5665978e64c740bfa49fc66
Alyxandra Ferrari <alyx@alyxandra.io>
2025-11-15 00:27:19 -0500

    First version working perfectly on `arm64-iphoneos` !! (yippee!!!)
    ^O^

--- sources/libkernel/xfbu/info/xfbu_compilation_date.c
+++ sources/libkernel/xfbu/info/xfbu_compilation_date.c
@@ -1,4 +1,4 @@
 #include <libkernel/xfbu/info.h>
 
 // populated by Make invocation
-const char* XFBU_COMPILATION_DATE = "2025-11-14 22:21:22";
+const char* XFBU_COMPILATION_DATE = "2025-11-15 00:19:33";
